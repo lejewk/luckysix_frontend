ce461bbcbde7398892346f08337b0105cc3c805f
lighten518 <lejewk@naver.com>
2020-09-06 16:17:36 +0900

    ㄱㄱ

--- src/components/Lottery.vue
+++ src/components/Lottery.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <div>제 ㅇㅇㅇ 회</div>
+  <div id="draws">
+    <div>제 {{round}} 회 당첨번호</div>
     <div>-----------------</div>
-    <div>01 11 25 31 37 42</div>
+    <div>{{printDraws}}</div>
+    <div>-----------------</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Lottery"
+  name: "Lottery",
+  data() {
+    return {
+      round: 927,
+      draws: [3,7,11,37,41,44]
+    }
+  },
+  computed: {
+    printDraws: function() {
+      return this.draws.map(function(it) {
+        return it.toString().padStart(2, '0');
+      }).join(" ");
+    }
+  }
 }
 </script>
+
+<style scoped>
+#draws {
+  margin:10px;
+}
+</style>
