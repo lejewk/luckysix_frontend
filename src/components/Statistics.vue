27bb7803f440558455c33574605f99e2e7e1ebf7
lighten518 <lejewk@naver.com>
2020-09-06 22:05:49 +0900

    데이터 출력 개선

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
     <div>번호별 당첨 통계</div>
-    <div>44 [■■■■■■■■■■■■■] 45%</div>
-    <div>11 [■■■■■■■■■■■] 40%</div>
-    <div>32 [■■■■■■■] 37%</div>
-    <div>05 [■■■■] 34%</div>
-    <div>42 [■■] 32%</div>
-    <div>21 [■] 21%</div>
+    <div v-for="article in top6" :key="article.no">
+      {{printDraws(article.no)}} {{printRateBar(article.rate)}} {{printRate(article.rate)}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Statistics'
+  name: 'Statistics',
+  data() {
+    return {
+      top6: [
+        {no: 44, rate: 45},
+        {no: 11, rate: 40},
+        {no: 32, rate: 37},
+        {no: 5, rate: 34},
+        {no: 42, rate: 32},
+        {no: 21, rate: 21}
+      ]
+    }
+  },
+  methods: {
+    printDraws: function(no) {
+      return no.toString().padStart(2, '0');
+    },
+    printRate: function(rate) {
+      return rate.toString() + '%';
+    },
+    printRateBar: function(rate) {
+      var topRate = this.top6[0].rate;
+      var maxBarSize = 10;
+
+      var barCount = maxBarSize * rate / topRate;
+      var rateBar = "";
+      for(var i=0; i<barCount; i++) {
+        rateBar += "■";
+      }
+
+      return rateBar;
+    }
+  }
 }
 </script>
