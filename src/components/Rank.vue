ae5d8fc0af8f3ac4dabd4c1179a22eb0461bcb0e
lighten518 <lejewk@naver.com>
2021-01-22 10:49:01 +0900

    심플 테이블로 변경

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -3,26 +3,36 @@
     <v-row no-gutters>
       <v-col cols="12">
         <v-card>
-          <v-data-table
-            dense
-            :headers="headers"
-            :header-props="headerProps"
-            :items="drawRates"
-            disable-pagination
-            hide-default-footer
+          <v-simple-table
+            fixed-header
+            height="100%"
           >
-            <template v-slot:[`item.no`]="{ item }">
-              <v-chip
-                :color="getColor(item.no)"
-                dark
-              >
-                {{ item.no }}
-              </v-chip>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    번호
+                  </th>
+                  <th class="text-left">
+                    횟수
+                  </th>
+                  <th class="text-left">
+                    비율
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="item in drawRates"
+                  :key="item.no"
+                >
+                  <td><v-chip :color="getColor(item.no)">{{ item.no }}</v-chip></td>
+                  <td>{{ item.count }}</td>
+                  <td>{{ item.rate }}%</td>
+                </tr>
+              </tbody>
             </template>
-            <template v-slot:[`item.rate`]="{ item }">
-                {{ item.rate }}%
-            </template>
-          </v-data-table>
+          </v-simple-table>
         </v-card>
       </v-col>
     </v-row>
@@ -36,22 +46,7 @@ export default {
   name: 'Rank',
   data() {
     return {
-      headerProps: {
-        sortByText: "정렬 기준"
-      },
-      headers: [
-        { text: '번호', value: 'no' },
-        { text: '횟수', value: 'count' },
-        { text: '비율', value: 'rate' }
-      ],
-      drawRates: [
-        {no: 1, count: 0, rate: 0},
-        {no: 17, count: 0, rate: 0},
-        {no: 26, count: 0, rate: 0},
-        {no: 32, count: 0, rate: 0},
-        {no: 40, count: 0, rate: 0},
-        {no: 42, count: 0, rate: 0}
-      ]
+      drawRates: []
     }
   },
   created() {
